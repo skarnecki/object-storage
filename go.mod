54ee34b226a258a71248ebfc929c0514cdc47757
Szymon Karnecki <ext.skarnecki@riotgames.com>
2022-04-24 12:46:44 +0200

    Locking version of minio, fixing deprecation warnings

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/spacelift-io/homework-object-storage
 
 go 1.15
+
+require (
+	github.com/containerd/containerd v1.6.2 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.14+incompatible
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	golang.org/x/time v0.0.0-20220411224347-583f2d630306 // indirect
+	google.golang.org/grpc v1.46.0 // indirect
+	gotest.tools/v3 v3.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+)
 
 func main() {
-	log.Println("Hello world!!!")
+	const ContainerName = "amazin-object-storage-node"
+	
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	//Filters for running, healthy containers with name amazin-object-storage-node-[0-9]
+	filters := filters.NewArgs(
+		filters.KeyValuePair{Key: "name", Value: ContainerName},
+		filters.KeyValuePair{Key: "status", Value: "running"},
+		//filters.KeyValuePair{Key: "health", Value: "healthy"}, //FIXME add healthchecks
+		)
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{Limit: 10, Filters: filters})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		details, err := cli.ContainerInspect(context.Background(), container.ID)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", details.NetworkSettings)
+	}
 }
